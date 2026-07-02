abdefeb3296c1ff854ebaaaed4069c94b8747b14
devlearner2804 <s26009@gsm.hs.kr>
2026-07-02 23:02:17 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/voidgsm/Feesh_Back/FeeshBackApplication.java
@@ -0,0 +1,13 @@
+package com.voidgsm.Feesh_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeshBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeshBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voidgsm/Feesh_Back/FeeshBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voidgsm.Feesh_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeshBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
