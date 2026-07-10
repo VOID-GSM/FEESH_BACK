30a1c29e9b69023c9151258f928ba4b10902c058
1ub7 <s26057@gsm.hs.kr>
2026-07-10 14:51:43 +0900

    feat: repository method 추가

--- src/main/java/com/feesh/post/repository/PostRepository.java
+++ src/main/java/com/feesh/post/repository/PostRepository.java
@@ -1,7 +1,12 @@
 package com.feesh.post.repository;
 
 import com.feesh.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
+    Page<Post> findAllByOrderByLikeCountDesc(Pageable pageable);
+    Page<Post> findByTitleContaining(String keyword, Pageable pageable);
 }
