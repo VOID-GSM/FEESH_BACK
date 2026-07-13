439cc7a44dd790452368d3fcfcafd721ac444b73
1ub7 <s26057@gsm.hs.kr>
2026-07-13 15:38:45 +0900

    feat: Comment Entity 및 CommentRepository 추가

---
+++ src/main/java/com/feesh/domain/post/entity/Comment.java
@@ -0,0 +1,39 @@
+package com.feesh.domain.post.entity;
+
+import com.feesh.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Builder
+    public Comment(String content, User author, Post post) {
+        this.content = content;
+        this.author = author;
+        this.post = post;
+        this.createdAt = LocalDateTime.now();
+    }
+}
