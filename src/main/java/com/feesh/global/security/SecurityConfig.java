8b868baa62b35129d2a9248bbafc575606160483
devlearner2804 <s26009@gsm.hs.kr>
2026-07-24 21:03:39 +0900

    fix: SecurityConfig 문법 오류 수정

--- src/main/java/com/feesh/global/security/SecurityConfig.java
+++ src/main/java/com/feesh/global/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/posts", "/posts/**").permitAll()
                 .anyRequest().authenticated()
 )
-                )
+                );
                 .addFilterBefore(
                         new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class
