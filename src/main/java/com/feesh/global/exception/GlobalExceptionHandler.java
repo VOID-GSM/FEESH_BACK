2424ddccb3a3176117ac6b18012632192ab08536
devlearner2804 <s26009@gsm.hs.kr>
2026-07-25 19:54:33 +0900

    fix: add exception logging

--- src/main/java/com/feesh/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/feesh/global/exception/GlobalExceptionHandler.java
@@ -4,7 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -36,14 +38,16 @@ public class GlobalExceptionHandler {
 
     // 그 외 예상 못한 예외 (서버 에러)
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        ErrorResponse response = ErrorResponse.builder()
-                .errorCode("INTERNAL_SERVER_ERROR")
-                .message("서버 내부 오류가 발생했습니다.")
-                .build();
+       public ResponseEntity<ErrorResponse> handleException(Exception e) {
+                log.error("처리되지 않은 예외 발생", e);
 
-        return ResponseEntity
-                .internalServerError()
-                .body(response);
-    }
+                ErrorResponse response = ErrorResponse.builder()
+                        .errorCode("INTERNAL_SERVER_ERROR")
+                        .message("서버 내부 오류가 발생했습니다.")
+                        .build();
+
+                return ResponseEntity
+                        .internalServerError()
+                        .body(response);
+        }
 }
